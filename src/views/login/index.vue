e7732b188a70f776abdca3e495127a6f5c1e5e37
lipengzhou <lpzmail@163.com>
2020-05-09 16:30:01 +0800

    登录注册-页面布局-样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,33 +1,41 @@
 <template>
   <div class="login-container">
+    <!-- 导航栏 -->
     <van-nav-bar
-      title="登录"
+      class="app-nav-bar"
+      title="注册 / 登录"
       left-arrow
       @click-left="$router.back()"
     />
+    <!-- /导航栏 -->
 
     <!-- 登录表单 -->
     <van-cell-group>
       <van-field
         v-model="user.mobile"
-        left-icon="smile-o"
+        icon-prefix="toutiao"
+        left-icon="shouji"
         placeholder="请输入手机号"
       />
       <van-field
         v-model="user.code"
         clearable
-        left-icon="music-o"
+        icon-prefix="toutiao"
+        left-icon="yanzhengma"
         placeholder="请输入验证码"
       >
         <template #button>
           <van-button
-            size="small"
+            class="send-btn"
+            size="mini"
             round
           >发送验证码</van-button>
         </template>
       </van-field>
     </van-cell-group>
-    <van-button type="info" block>登录</van-button>
+    <div class="login-btn-wrap">
+      <van-button class="login-btn" type="info" block>登录</van-button>
+    </div>
     <!-- /登录表单 -->
   </div>
 </template>
@@ -53,4 +61,26 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.login-container {
+  .send-btn {
+    width: 76px;
+    height: 23px;
+    background-color: #ededed;
+    .van-button__text {
+      font-size: 11px;
+      color: #666666;
+    }
+  }
+  .login-btn-wrap {
+    padding: 26px 16px;
+    .login-btn {
+      background-color: #6db4fb;
+      border: none;
+      .van-button__text {
+        font-size: 15px;
+      }
+    }
+  }
+}
+</style>
