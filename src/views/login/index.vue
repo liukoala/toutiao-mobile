7bfb8f00f8235c28f09fad3207d8a96e0871ec2e
lipengzhou <lpzmail@163.com>
2020-05-09 15:58:16 +0800

    登录注册-页面布局-结构

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="login-container">登录页面</div>
+  <div class="login-container">
+    <van-nav-bar
+      title="登录"
+      left-arrow
+      @click-left="$router.back()"
+    />
+
+    <!-- 登录表单 -->
+    <van-cell-group>
+      <van-field
+        v-model="user.mobile"
+        left-icon="smile-o"
+        placeholder="请输入手机号"
+      />
+      <van-field
+        v-model="user.code"
+        clearable
+        left-icon="music-o"
+        placeholder="请输入验证码"
+      >
+        <template #button>
+          <van-button
+            size="small"
+            round
+          >发送验证码</van-button>
+        </template>
+      </van-field>
+    </van-cell-group>
+    <van-button type="info" block>登录</van-button>
+    <!-- /登录表单 -->
+  </div>
 </template>
 
 <script>
@@ -8,7 +38,12 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '', // 手机号
+        code: '' // 验证码
+      }
+    }
   },
   computed: {},
   watch: {},
