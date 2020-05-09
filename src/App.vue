54e5f8de17ce4daef264a160e2cf49223971637f
lipengzhou <lpzmail@163.com>
2020-05-09 10:47:46 +0800

    项目初始化-导入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
     <h1>黑马头条</h1>
     <!-- 路由的出口 -->
     <router-view/>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -6,6 +6,15 @@ import store from './store'
 // 加载全局样式
 import './styles/index.less'
 
+// 加载 Vant 组件库
+import Vant from 'vant'
+
+// 加载 Vant 组件库样式
+import 'vant/lib/index.css'
+
+// 全局注册 Vant 中的组件
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 // 创建 Vue 根实例，将 router、store 配置到根实例中
