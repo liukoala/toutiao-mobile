9f9d229fd33bfb2b23b488ced55ec7cdcc69e39a
lipengzhou <lpzmail@163.com>
2020-05-10 15:54:34 +0800

    我的-页面布局-头部用户信息

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="my-container">我的页面</div>
+  <div class="my-container">
+    <van-cell-group>
+      <van-cell
+        title="单元格"
+        value="内容"
+        center
+      >
+        <van-image
+          slot="icon"
+          width="50"
+          height="50"
+          round
+          fit="cover"
+          src="https://img.yzcdn.cn/vant/cat.jpeg"
+        />
+        <div slot="title">昵称</div>
+        <van-button
+          size="small"
+          round
+        >编辑资料</van-button>
+      </van-cell>
+    </van-cell-group>
+  </div>
 </template>
 
 <script>
