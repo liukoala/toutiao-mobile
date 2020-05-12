6b7644687586e2b37bb5277cf58b33550884cad5
lipengzhou <lpzmail@163.com>
2020-05-12 11:26:38 +0800

    我的-展示当前登录用户信息

--- src/api/user.js
+++ src/api/user.js
@@ -3,6 +3,10 @@
  */
 import request from '@/utils/request'
 
+// 在非组件模块中获取 store 必须通过这种方式
+// 这里单独加载 store，和在组件中 this.$store 一个东西
+import store from '@/store/'
+
 /**
  * 登录/注册
  */
@@ -21,5 +25,18 @@ export const sendSms = mobile => {
   return request({
     method: 'GET',
     url: `/app/v1_0/sms/codes/${mobile}`
+  })
+}
+
+/**
+ * 获取登录用户信息
+ */
+export const getCurrentUser = () => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/user',
+    headers: {
+      Authorization: `Bearer ${store.state.user.token}`
+    }
   })
 }
--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -3,8 +3,6 @@
     <van-cell-group v-if="user" class="my-info">
       <van-cell
         class="base-info"
-        title="单元格"
-        value="内容"
         center
         :border="false"
       >
@@ -13,9 +11,9 @@
           slot="icon"
           round
           fit="cover"
-          src="https://img.yzcdn.cn/vant/cat.jpeg"
+          :src="currentUser.photo"
         />
-        <div class="name" slot="title">昵称</div>
+        <div class="name" slot="title">{{ currentUser.name }}</div>
         <van-button
           class="update-btn"
           size="small"
@@ -25,25 +23,25 @@
       <van-grid class="data-info" :border="false">
         <van-grid-item class="data-info-item">
           <div slot="text" class="text-wrap">
-            <div class="count">123</div>
+            <div class="count">{{ currentUser.art_count }}</div>
             <div class="text">头条</div>
           </div>
         </van-grid-item>
         <van-grid-item class="data-info-item">
           <div slot="text" class="text-wrap">
-            <div class="count">123</div>
+            <div class="count">{{ currentUser.follow_count }}</div>
             <div class="text">关注</div>
           </div>
         </van-grid-item>
         <van-grid-item class="data-info-item">
           <div slot="text" class="text-wrap">
-            <div class="count">123</div>
+            <div class="count">{{ currentUser.fans_count }}</div>
             <div class="text">粉丝</div>
           </div>
         </van-grid-item>
         <van-grid-item class="data-info-item">
           <div class="text-wrap" slot="text">
-            <div class="count">123</div>
+            <div class="count">{{ currentUser.like_count }}</div>
             <div class="text">获赞</div>
           </div>
         </van-grid-item>
@@ -85,21 +83,31 @@
 
 <script>
 import { mapState } from 'vuex'
+import { getCurrentUser } from '@/api/user'
 
 export default {
   name: 'MyIndex',
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      currentUser: {} // 当前登录用户信息
+    }
   },
   computed: {
     ...mapState(['user'])
   },
   watch: {},
-  created () {},
+  created () {
+    this.loadCurrentUser()
+  },
   mounted () {},
   methods: {
+    async loadCurrentUser () {
+      const { data } = await getCurrentUser()
+      this.currentUser = data.data
+    },
+
     onLogout () {
       // 提示用户确认退出
       // 确认 -> 处理退出
