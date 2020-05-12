9546ec756eea0af8166a080cf236e4373445eef0
lipengzhou <lpzmail@163.com>
2020-05-12 11:35:40 +0800

    我的-使用请求拦截器统一设置token

--- src/api/user.js
+++ src/api/user.js
@@ -3,10 +3,6 @@
  */
 import request from '@/utils/request'
 
-// 在非组件模块中获取 store 必须通过这种方式
-// 这里单独加载 store，和在组件中 this.$store 一个东西
-import store from '@/store/'
-
 /**
  * 登录/注册
  */
@@ -34,9 +30,9 @@ export const sendSms = mobile => {
 export const getCurrentUser = () => {
   return request({
     method: 'GET',
-    url: '/app/v1_0/user',
-    headers: {
-      Authorization: `Bearer ${store.state.user.token}`
-    }
+    url: '/app/v1_0/user'
+    // headers: {
+    //   Authorization: `Bearer ${store.state.user.token}`
+    // }
   })
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -3,11 +3,31 @@
  */
 import axios from 'axios'
 
+// 在非组件模块中获取 store 必须通过这种方式
+// 这里单独加载 store，和在组件中 this.$store 一个东西
+import store from '@/store/'
+
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/' // 基础路径
 })
 
 // 请求拦截器
+// Add a request interceptor
+request.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  const { user } = store.state
+
+  // 如果用户已登录，统一给接口设置 token 信息
+  if (user) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+
+  // 处理完之后一定要把 config 返回，否则请求就会停在这里
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
+})
 
 // 响应拦截器
 
